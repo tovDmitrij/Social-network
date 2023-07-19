1004762ae92e94672fcdd59fb7b74a09b5b2b374
Dmitrij Kuzmin <86602542+tovDmitrij@users.noreply.github.com>
2023-07-19 14:49:31 +0300

    Начало переписывания проекта с полного нуля

--- app/api/components/benchmark.performance/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using benchmark.performance;
-using BenchmarkDotNet.Running;
-
-DateParser parser = new DateParser();
-parser.GetYearFromSpanWithManualConversion("2019-12-13T16:33:06Z");
-
-//BenchmarkRunner.Run<DateParserBenchmarks>();
-//Console.ReadKey();
