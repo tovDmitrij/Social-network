db396cbb123bad578a849704dcb8f9331c00bafe
Dmitrij Kuzmin <86602542+tovDmitrij@users.noreply.github.com>
2023-07-19 20:11:10 +0300

    Патч -Реализовал скрипт для создания единой БД в постгрес; -Создал примерные докер и композ файлы.

--- app/database/scripts/sql/db.logs/create.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists app_error_logs(
-	id			serial			primary key,
-	message		text not null,
-	source 		text not null,
-	stack_trace text not null,
-	date		timestamp default current_timestamp not null
-);
